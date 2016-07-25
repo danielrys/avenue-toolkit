5c81a979d545748d723ec7abf65b4b0203d8b066
Daniel Rys <rys@usertechnologies.com>
2016-07-25 15:47:51 +0200

    simplified wrapper file structure

--- avenue-toolkit.ts
+++ avenue-toolkit.ts
@@ -1,7 +1,2 @@
-import {AccordionItem} from './components/accordion-item/accordion-item';
-import {Accordion} from './directives/accordion/accordion';
-
-export let AvenueToolkit = {
-	accordionItem: AccordionItem,
-	accordion: Accordion
-}
+export * from './components/accordion-item/accordion-item';
+export * from './directives/accordion/accordion';
